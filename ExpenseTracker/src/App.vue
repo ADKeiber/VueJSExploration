d49937a8721be795ba012d85e1fbdd5a407d0ab4
Austin Keiber <adkeiber@gmail.com>
2025-03-21 20:33:08 -0400

    add login modal

--- ExpenseTracker/src/App.vue
+++ ExpenseTracker/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+  import LoginModal from './components/LoginModal.vue'
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">HELLO WORLD!</h1>
+  <LoginModal></LoginModal>
 </template>
 
 <style scoped>
---
+++ ExpenseTracker/src/components/LoginModal.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Login Module</h1>
+</template>
+
+<style scoped>
+export default {
+  name: "LoginModal"
+}
+</style>
