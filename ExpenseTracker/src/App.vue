8f29975ededf93ee67214262643b54ee36df5586
Austin Keiber <adkeiber@gmail.com>
2025-03-21 20:44:07 -0400

    add expense dashboard

--- ExpenseTracker/src/App.vue
+++ ExpenseTracker/src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
   import LoginModal from './components/LoginModal.vue'
+  import ExpenseDashboard from './components/ExpenseDashboard.vue'
+  const loggedIn = ref(false)
+
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">HELLO WORLD!</h1>
-  <LoginModal></LoginModal>
+  
+  <LoginModal v-if="loggedIn"></LoginModal>
+  <ExpenseDashboard v-else></ExpenseDashboard>
 </template>
 
 <style scoped>
