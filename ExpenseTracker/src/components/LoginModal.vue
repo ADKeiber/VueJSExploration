4542e56656352c58e2a847c9733ac5df4aea4d8e
Austin Keiber <adkeiber@gmail.com>
2025-03-26 20:51:37 -0400

    login

--- ExpenseTracker/src/components/LoginModal.vue
+++ ExpenseTracker/src/components/LoginModal.vue
@@ -3,24 +3,28 @@
 </script>
 
 <template>
-  <div class="bg-white p-6 rounded-lg shadow-lg w-1/3 h-3/6 flex justify-center items-center flex-col">
-    <div class="flex justify-between items-center mb-4">
-      <h2 class="text-xl font-semibold">Login</h2>
+  <div class="bg-white p-6 rounded-lg shadow-lg w-1/3 h-4/6 flex justify-around items-center flex-col">
+
+    <div class="flex justify-between items-center mb-4 h-1/6">
+      <h2 class="text-4xl font-semibold">Login</h2>
     </div>
     
-    <form class="w-full max-w-100">
+    <form class="w-full max-w-100 h-4/6">
       <div class="mb-4">
         <label class="block text-gray-700">Email</label>
         <input type="email" class="w-full p-2 border rounded focus:ring-2 focus:ring-blue-500" placeholder="Enter email">
       </div>
+
       <div class="mb-4">
         <label class="block text-gray-700">Password</label>
         <input type="password" class="w-full p-2 border rounded focus:ring-2 focus:ring-blue-500" placeholder="Enter password">
       </div>
+
       <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
         Login
       </button>
     </form>
+
   </div>
 </template>
 
