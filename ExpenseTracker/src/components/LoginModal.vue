caaf52547fd04669e8e7c40507a0705b639968ec
Austin Keiber <adkeiber@gmail.com>
2025-03-26 21:05:38 -0400

    more formatting

--- ExpenseTracker/src/components/LoginModal.vue
+++ ExpenseTracker/src/components/LoginModal.vue
@@ -10,17 +10,17 @@
     </div>
     
     <form class="w-full max-w-100 h-4/6">
-      <div class="mb-4">
+      <div class="mb-6 text-xl">
         <label class="block text-gray-700">Email</label>
         <input type="email" class="w-full p-2 border rounded focus:ring-2 focus:ring-blue-500" placeholder="Enter email">
       </div>
 
-      <div class="mb-4">
+      <div class="mb-6 text-xl">
         <label class="block text-gray-700">Password</label>
         <input type="password" class="w-full p-2 border rounded focus:ring-2 focus:ring-blue-500" placeholder="Enter password">
       </div>
 
-      <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
+      <button type="submit" class="w-full bg-slate-400 text-white py-2 rounded hover:bg-slate-500">
         Login
       </button>
     </form>
