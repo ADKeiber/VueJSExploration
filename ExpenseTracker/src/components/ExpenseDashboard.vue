f9f05f728be04144d138d9d8ff8b5d891a903d8c
Austin Keiber <adkeiber@gmail.com>
2025-03-29 23:59:53 -0400

    add expense area

--- ExpenseTracker/src/components/ExpenseDashboard.vue
+++ ExpenseTracker/src/components/ExpenseDashboard.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Expense Dashboard</h1>
+  <div id="expenseArea" class="size-9/10 rounded-lg">
+    <h1 class="text-3xl font-bold underline">Expense Dashboard</h1>
+  </div>
+  
 </template>
 
 <style scoped>
